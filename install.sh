0d61ff820759f70ef0aa017e7dfa4af399a2264c
gfw-breaker <lixiaoming0513@gmail.com>
2021-07-19 19:43:59 +0800

    ok

--- install.sh
+++ install.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
 
-pkg=https://github.com/fatedier/frp/releases/download/v0.37.0/frp_0.37.0_linux_amd64.tar.gz
-
-
-# set config
-cd /root/install-frps
+# deploy 
 mkdir -p /etc/frp/
 cp frps.ini /etc/frp/
+chmod +x frps; cp frps /usr/bin/
+cp frps.service /etc/systemd/system/
 
+# config
 source /tmp/frp_params.txt
 cat >> /etc/frp/frps.ini << EOF
 dashboard_pwd = $frp_password
 token = $frp_token
 EOF
-
-# deploy binary
-cd /tmp
-wget $pkg
-tar xvf frp*.gz
-cp frp*/frps /usr/bin/
-cp frp*/systemd/frps.service /etc/systemd/system
 
 
 # start service
